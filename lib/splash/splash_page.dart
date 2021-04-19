a5fb5099e63f66f74b18274d70423efc92278dbf
Matheus <matheusmitestainer@outlook.com>
2021-04-19 16:51:40 -0300

    NLW#5 - Day 1

---
+++ lib/splash/splash_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:devquiz/core/app_gradients.dart';
+import 'package:devquiz/core/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: (BoxDecoration(gradient: AppGradients.linear)),
+        child: Center(child: Image.asset(AppImages.logo)),
+      ),
+    );
+  }
+}
