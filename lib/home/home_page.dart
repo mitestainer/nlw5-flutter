e0c022a766e9a5d9241e1000479a37b79b2fe0ff
Matheus <matheusmitestainer@outlook.com>
2021-04-20 16:42:13 -0300

    NLW#5 - Day 2

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:devquiz/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +13,38 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBarWidget());
+    return Scaffold(
+        appBar: AppBarWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    LevelButtonWidget(label: 'Fácil'),
+                    LevelButtonWidget(label: 'Médio'),
+                    LevelButtonWidget(label: 'Difícil'),
+                    LevelButtonWidget(label: 'Perito'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                  children: [
+                    QuizCardWidget(),
+                    QuizCardWidget(),
+                    QuizCardWidget()
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
